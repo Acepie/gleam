fee5abe2d90dcbb872cd11db0098110d4ed0a6ff
Ameen Radwan <ameen_radwan@intuit.com>
2024-08-02 15:43:29 -0700

    lints

--- compiler-core/src/type_/environment.rs
+++ compiler-core/src/type_/environment.rs
@@ -556,13 +556,12 @@ impl<'a> Environment<'a> {
     ) {
         self.increment_usage(name);
         if module != &self.current_module {
-            let _ = self
-                .imported_module_value_usages
+            self.imported_module_value_usages
                 .entry(module.clone())
-                .or_insert_with(HashMap::new)
+                .or_default()
                 .entry(name.clone())
-                .or_insert_with(Vec::new)
-                .push(location.clone());
+                .or_default()
+                .push(*location);
         }
     }
 
@@ -574,13 +573,12 @@ impl<'a> Environment<'a> {
     ) {
         self.increment_usage(name);
         if module != &self.current_module {
-            let _ = self
-                .imported_module_type_usages
+            self.imported_module_type_usages
                 .entry(module.clone())
-                .or_insert_with(HashMap::new)
+                .or_default()
                 .entry(name.clone())
-                .or_insert_with(Vec::new)
-                .push(location.clone());
+                .or_default()
+                .push(*location);
         }
     }
 
